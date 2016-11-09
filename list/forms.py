fcda2db9672c684c3c2d267a1b71fb12289a75c0
Marte Løge <marte.loge@gmail.com>
2016-11-09 23:38:47 +0100

    fix: rendering of image, url and location

--- list/forms.py
+++ list/forms.py
@@ -4,7 +4,7 @@ from .models import Item, List
 class AddItem(forms.ModelForm):
     class Meta:
         model = Item
-        fields = ('name', 'url', 'price_range', 'description','image',)
+        fields = ('name', 'url', 'location', 'price_range', 'description','image',)
 
 class BuyItem(forms.ModelForm):
     class Meta:
