c50180f2c9d5f3133cd376739f6e54809cff36f3
Marte Løge <marte.loge@gmail.com>
2016-11-14 22:58:32 +0100

    fix: admin view

--- list/admin.py
+++ list/admin.py
@@ -3,5 +3,11 @@ from django.contrib import admin
 from .models import List, Item
 from registration.models import RegistrationProfile
 
-admin.site.register(List)
-admin.site.register(Item)
+class ListAdmin(admin.ModelAdmin):
+    list_display = ('user', 'id', 'name')
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('id', 'list', 'name', 'bought')
+
+admin.site.register(List, ListAdmin)
+admin.site.register(Item, ItemAdmin)
