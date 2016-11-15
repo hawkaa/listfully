341d862c5dd5dd17ccfda4ce75d85a438e20f0a1
Marte Løge <marte.loge@gmail.com>
2016-11-16 00:39:44 +0100

    fix: image orientation

--- list/models.py
+++ list/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User as auth_user
-from PIL import Image
+from PIL import Image, ExifTags
 
 def item_image_path(instance, filename):
     return ('item_images/' + filename).format(auth_user.id, instance.id)
@@ -29,19 +29,37 @@ class Item(models.Model):
     location = models.CharField(max_length=100, blank=True)
 
     def save(self):
-          super(Item, self).save()
-          if self.image:
-              base_width = 550
-              img = Image.open(self.image.path)
-              base = Image.new(mode='RGBA',size=(base_width,base_width),color=(255,255,255,0))
-              width, height = img.size
+        super(Item, self).save()
+        if self.image:
+            img = Image.open(self.image.path)
 
-              #SCALE IMAGE
-              width_percent = (base_width/float(width))
-              new_height = int((float(height)*float(width_percent)))
-              img = img.resize((base_width, new_height), Image.ANTIALIAS)
-              img.save(self.image.path, 'PNG')
+            try:
+                for orientation in ExifTags.TAGS.keys():
+                  if ExifTags.TAGS[orientation]=='Orientation':
+                      break
+                exif = dict(img._getexif().items())
+                if exif[orientation] == 3:
+                  img=img.rotate(180, expand=True)
+                  print(3)
+                elif exif[orientation] == 6:
+                  img=img.rotate(270, expand=True)
+                  print(6)
+                elif exif[orientation] == 8:
+                  img=img.rotate(90, expand=True)
+                  print(8)
+            except (AttributeError, KeyError, IndexError):
+                pass
 
-              #CROP HORIZONTAL IMAGES MORE?
-              base.paste(img, (0, int((550/2 - img.size[1]/2))))
-              base.save(self.thumbnail.path, 'PNG')
+            base_width = 550
+            base = Image.new(mode='RGBA',size=(base_width,base_width),color=(255,255,255,0))
+            width, height = img.size
+
+            #SCALE IMAGE
+            width_percent = (base_width/float(width))
+            new_height = int((float(height)*float(width_percent)))
+            img = img.resize((base_width, new_height), Image.ANTIALIAS)
+            img.save(self.image.path, 'PNG')
+
+            #CROP HORIZONTAL IMAGES MORE?
+            base.paste(img, (0, int((550/2 - img.size[1]/2))))
+            base.save(self.thumbnail.path, 'PNG')
